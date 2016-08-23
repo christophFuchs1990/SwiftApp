eda70e97160bf2a0be75d6140ae9ed2f3f86c30b
Christoph Fuchsjäger <ChristophFuchsjaeger@christophs-macbook-pro.local>
2016-08-23 22:08:29 +0200

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Christoph Fuchsjäger on 23.08.16.
+//  Copyright © 2016 Christoph Fuchsjäger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
